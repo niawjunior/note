e379487675ee2cc65916fef17a78f951c62378ff
niawjunior <pasupol@dome.cloud>
2019-08-04 02:35:12 +0700

    update

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -2,16 +2,28 @@
   <div>
      <md-table>
       <md-table-row>
-        <md-table-head>ชื่อบทความ</md-table-head>
+        <md-table-head>วันที่/เวลา</md-table-head>
+        <md-table-head>ชื่อโน๊ต</md-table-head>
         <md-table-head>คำอธิบาย</md-table-head>
         <md-table-head>หมวดหมู่</md-table-head>
+        <md-table-head>รูปแบบ</md-table-head>
         <md-table-head>ลิ้งค์</md-table-head>
       </md-table-row>
       <md-table-row>
-        <md-table-cell>JavaScript</md-table-cell>
+        <md-table-cell>02/06/2019 12:32</md-table-cell>
+        <md-table-cell>JavaScript พื้นฐาน</md-table-cell>
         <md-table-cell>สอนเขียน JavaScript</md-table-cell>
         <md-table-cell>JavaScript</md-table-cell>
-        <md-table-cell>www.google.com</md-table-cell>
+        <md-table-cell>บทความ</md-table-cell>
+        <md-table-cell><a href="www.google.com">www.google.com</a></md-table-cell>
+      </md-table-row>
+      <md-table-row>
+        <md-table-cell>04/06/2019 09:10</md-table-cell>
+        <md-table-cell>Angular พื้นฐาน</md-table-cell>
+        <md-table-cell>สอนเขียน Angular</md-table-cell>
+        <md-table-cell>Angular</md-table-cell>
+        <md-table-cell>วีดีโอ</md-table-cell>
+        <md-table-cell><a href="www.youtube.com">www.youtube.com</a></md-table-cell>
       </md-table-row>
     </md-table>
   </div>
